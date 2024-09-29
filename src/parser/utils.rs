5b2a19c6a8f1ccadd58a2ce5e6a8d4e2bd2dc924
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2024-09-28 22:49:20 -0300

    wip: print parsed file

--- src/parser/utils.rs
+++ src/parser/utils.rs
@@ -2,10 +2,13 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
+use super::parse_statement::parse_statement;
+use super::PrologStatement;
+
 fn clean_line(line: &str) -> String {
     if let Some(index) = line.find('%') {
         line[..index].trim().to_string()
-    }else {
+    } else {
         line.trim().to_string()
     }
 }
@@ -14,14 +17,26 @@ pub fn parse_file(filepath: &str) -> io::Result<()> {
     let path = Path::new(&filepath);
     let file = File::open(&path)?;
     let reader = io::BufReader::new(file);
-    
-    for line in reader.lines(){
+
+    for line in reader.lines() {
         match line {
             Ok(content) => {
                 let cleaned_line = clean_line(&content);
 
-                if !cleaned_line.is_empty(){
-                    println!("{}", cleaned_line);
+                if !cleaned_line.is_empty() {
+                    if let Some(parsed_line) = parse_statement(&cleaned_line) {
+                        match parsed_line {
+                            PrologStatement::PrologFact(fact) => {
+                                println!("Parsed fact: {}", fact.predicate);
+                            }
+                            PrologStatement::PrologRule(rule) => {
+                                println!("Parsed rule: {}", rule);
+                                for predicate in &rule.body {
+                                    println!("  Body predicate: {}", predicate);
+                                }
+                            }
+                        }
+                    }
                 }
             }
 
