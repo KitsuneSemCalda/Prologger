32a12d7d62ad8080e72a08b00d41ce5b757a6ffa
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2024-10-05 13:27:33 -0300

    refactoring (improving comments clean): now the comments can be a multiline or a single line comments

--- src/parser/utils.rs
+++ src/parser/utils.rs
@@ -5,12 +5,44 @@ use std::path::Path;
 use super::parse_statement::parse_statement;
 use super::PrologStatement;
 
-fn clean_line(line: &str) -> String {
+fn line_comment(line: &str) -> String {
     if let Some(index) = line.find('%') {
         line[..index].trim().to_string()
     } else {
         line.trim().to_string()
     }
+}
+
+fn multiline_comment(line: &str) -> String {
+    let mut cleaned = String::new();
+    let mut in_block_comment = false;
+    let mut chars = line.chars().peekable();
+
+    while let Some(c) = chars.next() {
+        if in_block_comment {
+            if c == '*' && chars.peek() == Some(&'/') {
+                in_block_comment = false;
+                chars.next();
+            }
+
+            continue
+        }
+
+        if c == '/' && chars.peek() == Some(&'*') {
+            in_block_comment = true;
+            chars.next();
+            continue
+        }
+
+        cleaned.push(c);
+    }
+
+    cleaned.trim().to_string()
+}
+
+fn clean_line(line: &str) -> String {
+    let line_after_multiline_cleanup = multiline_comment(line);
+    line_comment(&line_after_multiline_cleanup)
 }
 
 pub fn parse_file(filepath: &str) -> io::Result<()> {
@@ -26,14 +58,9 @@ pub fn parse_file(filepath: &str) -> io::Result<()> {
                 if !cleaned_line.is_empty() {
                     if let Some(parsed_line) = parse_statement(&cleaned_line) {
                         match parsed_line {
-                            PrologStatement::PrologFact(fact) => {
-                                println!("Parsed fact: {}", fact.predicate);
+                            PrologStatement::PrologFact(_fact) => {
                             }
-                            PrologStatement::PrologRule(rule) => {
-                                println!("Parsed rule: {}", rule);
-                                for predicate in &rule.body {
-                                    println!("  Body predicate: {}", predicate);
-                                }
+                            PrologStatement::PrologRule(_rule) => {
                             }
                         }
                     }
